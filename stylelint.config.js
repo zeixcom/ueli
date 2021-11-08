517e3986c3e481caea49f1f6fa111b74900243d6
Sergio Lavanga <sergio.lavanga@zeix.com>
2021-11-08 14:58:35 +0100

    Update stylelint.config.js

--- stylelint.config.js
+++ stylelint.config.js
@@ -8,7 +8,6 @@ module.exports = {
     'at-rule-no-unknown': null,
     'scss/at-rule-no-unknown': true,
     'declaration-empty-line-before': null,
-    'no-invalid-position-at-import-rule': null,
     'order/order': [
       [
         {
